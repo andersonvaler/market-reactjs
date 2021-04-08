7b5639df890eda871923b9e091ba0778de0b188e
alexandre coelho <nasciantiontem@outlook.com>
2021-04-07 22:49:31 -0300

    feat(/src/components/Card_register): criando o componente de registro

---
+++ src/components/Card_register/index.js
@@ -0,0 +1,73 @@
+import { Button } from "../Button/PrimaryButton/style";
+import {
+  Container,
+  Header,
+  Box,
+  TextInput,
+  Input,
+  BoxLogin,
+  Link,
+} from "./style";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import api from "../../services/api";
+
+export const Register = () => {
+  const schema = yup.object().shape({
+    username: yup
+      .string("invalid")
+      .matches(/^[aA-zZ]+$/, "invalid")
+      .required("required"),
+  });
+
+  const { register, handleSubmit, errors, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const handleForm = (data) => {
+    api
+      .post("users/", data)
+      .then(() => {
+        console.log("ok");
+      })
+      .catch((e) => e.messages);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(handleForm)}>
+        <div>
+          <Container>
+            <Header>Cadastro</Header>
+            <Box>
+              <TextInput>Nome:</TextInput>
+              <Input />
+            </Box>
+            <Box>
+              <TextInput>Email:</TextInput>
+              <Input />
+            </Box>
+            <Box>
+              <TextInput>Endereço:</TextInput>
+              <Input />
+            </Box>
+            <Box>
+              <TextInput>Senha:</TextInput>
+              <Input />
+            </Box>
+            <Box>
+              <TextInput>Confirmar senha:</TextInput>
+              <Input />
+            </Box>
+            <BoxLogin>
+              ja possui uma conta?
+              <Link href="http://localhost:3000/"> faça o login</Link>
+            </BoxLogin>
+            <Button type="submit">Cadastrar</Button>
+          </Container>
+        </div>
+      </form>
+    </>
+  );
+};
+export default Register;
---
+++ src/components/Card_register/style.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 22rem;
+  height: 30rem;
+  background-color: #f3ecf9;
+  border-radius: 1rem;
+`;
+
+export const Header = styled.div`
+  font-size: 2rem;
+  color: #3a2552;
+  justify-content: center;
+  display: flex;
+`;
+
+export const Box = styled.div`
+  padding: 0.2rem;
+  padding-left: 1.5rem;
+`;
+
+export const TextInput = styled.div`
+  font-size: 1rem;
+  color: #927dae;
+`;
+
+export const Input = styled.input`
+  width: 19rem;
+  height: 2rem;
+  border-radius: 1rem;
+  padding-left: 1.5rem;
+  color: black;
+`;
+
+export const BoxLogin = styled.div`
+  padding: 1.8rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Link = styled.a`
+  color: #54cd87;
+`;
