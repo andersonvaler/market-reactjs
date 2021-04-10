fc1aa092718f247c5987347db2f3f9b4f77afea8
andersonvaler <andersonvaler@gmail.com>
2021-04-09 21:20:26 -0300

    feat(src/components/ModalCard/index.jsx): criado componente modal reutilizavel

---
+++ src/components/ModalCard/index.jsx
@@ -0,0 +1,91 @@
+/*
+Pra chamar este componente é necessário criar um state com valor false inicial 
+no componente onde for chamado e passar por prop, 
+ex: 
+const [open, setopen] = useState(false)
+<ModalCard open={open} setOpen ={setOpen}> Só colocar o conteudo aqui que vai entrar no modal </ModalCard>
+aí para abrir pode criar uma função que aciona setOpen(!open)
+*/
+import { forwardRef } from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import { useSpring, animated } from "react-spring/web.cjs";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    borderRadius: "8px",
+    padding: theme.spacing(2, 2),
+  },
+}));
+
+const Fade = forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
+
+const ModalCard = (props) => {
+  const { open, setOpen } = props;
+  const classes = useStyles();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>{props.children}</div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalCard;
