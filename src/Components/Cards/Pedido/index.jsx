328f945720b1ca801620614c08fdd5a7d681164a
andersonvaler <andersonvaler@gmail.com>
2021-04-17 23:00:09 -0300

    feat: detalhamento de orçamento

--- src/Components/Cards/Pedido/index.jsx
+++ src/Components/Cards/Pedido/index.jsx
@@ -1,14 +1,23 @@
-import { PedidoContainer } from "./style";
+import { PedidoContainer, ProdutosContainer } from "./style";
 import { Button } from "../../Button/PrimaryButton/style";
+import ProdutoCarrinho from "../ProdutoCarrinho";
 
 const Pedido = ({ pedido }) => {
-  console.log(pedido);
   return (
     <PedidoContainer>
       <h2>Detalhes do pedido</h2>
       <h4>Cliente: {pedido.name} </h4>
       <h4>Endereço: {pedido.adress}</h4>
       <h4>Telefone: {pedido.number}</h4>
+      <ProdutosContainer>
+        {pedido.products.map((produto, index) => {
+          return (
+            <>
+              <ProdutoCarrinho key={index} produto={produto}></ProdutoCarrinho>
+            </>
+          );
+        })}
+      </ProdutosContainer>
       <Button>Enviar orçamento</Button>
     </PedidoContainer>
   );
