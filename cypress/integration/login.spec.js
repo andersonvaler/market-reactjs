fd5a99b751390d1192f79d9022d134e529cc38eb
Ricardo Albuquerque <ricardoalbuquerquejunior12@gmail.com>
2021-04-18 19:33:28 -0300

    feat(cypress): Testes de criar usuario, fazer login e adicionar produto funcionando)

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,26 @@
+describe("Fazendo login", () => {
+  it("Fazer login do cliente", () => {
+    cy.visit("https://market-six.vercel.app/login/user");
+    cy.get('input[name="email"]').focus().type("clebinho@teste.com");
+    cy.get('input[name="password"]').focus().type(123456);
+    cy.contains("Entrar").click();
+    cy.wait(10000);
+  });
+
+  it("Pesquisando produtos", () => {
+    cy.contains("Buscar").click();
+    cy.wait(3000);
+    cy.contains("Carnes").click();
+    cy.wait(1500);
+    cy.get('input[placeholder="Procurar"]').focus().type("Picanha").click();
+    cy.get(".sc-eGPWxh").click();
+    cy.contains("Adicionar ao carrinho").click();
+    cy.get('input[type="number"]').type("5");
+    cy.contains("Confirmar").click();
+  });
+
+  it("Pedindo orçamento", () => {
+    cy.contains("Carrinho").click();
+    cy.contains("Solicitar Orçamento").click();
+  });
+});
